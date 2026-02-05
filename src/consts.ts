1e4afd99487592c241238778539873274bfb26f5
anatoly <anatoly@tkach.me>
2026-02-04 20:08:53 -0500

    chore: update site structure and styling

---
+++ src/consts.ts
@@ -0,0 +1,6 @@
+export const SITE_NAME = "ReaderPub";
+export const SITE_DOMAIN = "reader.pub";
+export const BOOKS_URL = "https://reader.pub/books/";
+export const WEREAD_NAME = "WeRead";
+export const BOOKTREE_NAME = "BookTree";
+export const PREVIEW_MODE = true;
